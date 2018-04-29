d65cf7c6ec68450e24c74ff837b5053493b575d4
Ariel Zhen <zhenpingping@live.com>
2018-04-29 06:03:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 function breakOut(array, changeValue, stopValue){
   for(var i = 0; i < array.length; i++){
     array[i] = changeValue;
-    
+
     if(array[i] === stopValue){
       break;
     }
   }
+  return array;
 }
 
 function keepGoing(array, changeValue, skipValue){
