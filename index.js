006a6b054cb8a03b28c26f6389fc89404a01e9dc
Ariel Zhen <zhenpingping@live.com>
2018-04-29 06:16:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function breakOut(array, changeValue, stopValue){
   for (let i = 0; i < array.length; i++){
-    if(array[i] == stopValue){
-      break;
+    if(array[i] === stopValue){
+      break
     }
     array[i] = changeValue;
   }
